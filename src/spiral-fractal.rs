bac82a63774515ccdce2d5fc1e54af6d48c46d07
VeryGoodUsername <57340908+Aitesedba@users.noreply.github.com>
2022-06-30 19:25:24 +0100

    Added speed limit

--- src/spiral-fractal.rs
+++ src/spiral-fractal.rs
@@ -10,47 +10,16 @@ fn main() {
         .run();
 }
 
-
-fn mouse_scroll(
-    mut mouse_wheel_events: EventReader<MouseWheel>,
-
-    mut query: Query<&mut OrthographicProjection, With<Camera>>,
-    mut speed: Local<f32>,
-) {
-    for event in mouse_wheel_events.iter() {
-        *speed -= event.y * 0.05;
-    }
-
-    for mut que in query.iter_mut() {
-        let mut log_scale = que.scale.ln();
-
-        log_scale += *speed;
-
-        println!("{}", log_scale);
-
-        // Make it jump back seamlessly, absolutelyno idea why this specific number works
-        if log_scale > 19.25 {
-            log_scale -= 19.25;
-        } else if log_scale < 0.0 {
-            log_scale += 19.25;
-        }
-
-
-        que.scale = log_scale.exp();
-    }
-}
-
 fn spiral_draw(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-
     // Relevant modifiers to info
     let shape_density = 20;
     let size_mod = 0.150;
-    
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     let mut prev_term = 0.0;
     let mut next_last_term = 1.0;
@@ -62,16 +31,13 @@ fn spiral_draw(
         Vec3::new(-1.0, 0.0, 0.0),
         Vec3::new(0.0, -1.0, 0.0),
     ];
-    
 
-    
-    for u in 0..=300 {
+    for u in 0..=100 {
         // Each segment is one quarter spiral
         let quarter = u as f32;
 
         // Calculate Fibbonacci sequence
         let mut radius = prev_term + next_last_term;
-        // outside_radius.0 = radius;
         next_last_term = prev_term;
         prev_term = radius;
 
@@ -83,15 +49,15 @@ fn spiral_draw(
             // Draw Quarter of spiral
 
             // The precise angle of individual shape, plus which quarter it is in.
-            let f =
-                ((((i as f32) / shape_density as f32) * 90.0) + quarter * -90.0 ).to_radians();
-
+            let f = ((((i as f32) / shape_density as f32) * 90.0) + (quarter * -90.0)).to_radians();
             commands.spawn_bundle(MaterialMesh2dBundle {
                 mesh: meshes
-                    .add(geometry::RegularPolygon::new(
-                            (prev_term+ (next_last_term * (1.0 - (i as f32 / shape_density as f32)))
-                                    )* size_mod
-                                    / 10.,
+                    .add(
+                        geometry::RegularPolygon::new(
+                            (prev_term
+                                + (next_last_term * (1.0 - (i as f32 / shape_density as f32))))
+                                * size_mod
+                                / 10.,
                             5,
                         )
                         .into(),
@@ -119,6 +85,38 @@ fn spiral_draw(
         if u == 0 {
             direction -= 1;
         }
-        // offset += change * radius;
+    }
+}
+
+fn mouse_scroll(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut speed: Local<f32>,
+) {
+    for event in mouse_wheel_events.iter() {
+        *speed -= event.y * 0.03;
+    }
+
+    if *speed > 19.25 {
+        *speed -= 19.25;
+    } else if *speed < 0.0 {
+        *speed += 19.25;
+    }
+
+    for mut que in query.iter_mut() {
+        let mut log_scale = que.scale.ln();
+
+        log_scale += *speed;
+        println!("Size {}, {}", log_scale, *speed);
+
+        // Make it jump back seamlessly, absolutely no idea why this specific number works
+        if log_scale > 19.25 {
+            log_scale -= 19.25;
+        } else if log_scale < 0.0 {
+            log_scale += 19.25;
+        }
+
+        que.scale = log_scale.exp();
     }
 }
