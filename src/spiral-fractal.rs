7c024d36bfcc22b6627a8888f89f2d53b880a78a
VeryGoodUsername <57340908+Aitesedba@users.noreply.github.com>
2022-06-30 19:33:33 +0100

    Added option for multiple spirals
    Edit the `total_spirals` variable to increase or decrease the amount of spirals rendered

--- src/spiral-fractal.rs
+++ src/spiral-fractal.rs
@@ -15,75 +15,81 @@ fn spiral_draw(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // Relevant modifiers to info
+    // Relevant modifiers to edit spiral properties
     let shape_density = 20;
     let size_mod = 0.150;
+    let total_spirals: u8 = 1;
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    for spiral_num in 0..total_spirals {
+        let mut prev_term = 0.0;
+        let mut next_last_term = 1.0;
+        let mut direction: usize = 0;
+        let mut offset: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+        let directions = [
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+            Vec3::new(-1.0, 0.0, 0.0),
+            Vec3::new(0.0, -1.0, 0.0),
+        ];
+        
+        for u in 0..=100 {
+            // Each segment is one quarter spiral
+            let quarter = u as f32;
 
-    let mut prev_term = 0.0;
-    let mut next_last_term = 1.0;
-    let mut direction: usize = 0;
-    let mut offset: Vec3 = Vec3::new(0.0, 0.0, 0.0);
-    let directions = [
-        Vec3::new(1.0, 0.0, 0.0),
-        Vec3::new(0.0, 1.0, 0.0),
-        Vec3::new(-1.0, 0.0, 0.0),
-        Vec3::new(0.0, -1.0, 0.0),
-    ];
+            // Calculate Fibbonacci sequence
+            let mut radius = prev_term + next_last_term;
+            next_last_term = prev_term;
+            prev_term = radius;
 
-    for u in 0..=100 {
-        // Each segment is one quarter spiral
-        let quarter = u as f32;
+            radius *= size_mod;
 
-        // Calculate Fibbonacci sequence
-        let mut radius = prev_term + next_last_term;
-        next_last_term = prev_term;
-        prev_term = radius;
+            let change = directions[direction] * next_last_term;
 
-        radius *= size_mod;
+            for i in 0..=shape_density {
+                // Draw Quarter of spiral
 
-        let change = directions[direction] * next_last_term;
-
-        for i in 0..=shape_density {
-            // Draw Quarter of spiral
-
-            // The precise angle of individual shape, plus which quarter it is in.
-            let f = ((((i as f32) / shape_density as f32) * 90.0) + (quarter * -90.0)).to_radians();
-            commands.spawn_bundle(MaterialMesh2dBundle {
-                mesh: meshes
-                    .add(
-                        geometry::RegularPolygon::new(
-                            (prev_term
-                                + (next_last_term * (1.0 - (i as f32 / shape_density as f32))))
-                                * size_mod
-                                / 10.,
-                            5,
+                // The precise angle of individual shape, plus which quarter it is in.
+                let f =
+                    ((((i as f32) / shape_density as f32) * 90.0) + (quarter * -90.0)).to_radians();
+                commands.spawn_bundle(MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(
+                            geometry::RegularPolygon::new(
+                                (prev_term
+                                    + (next_last_term * (1.0 - (i as f32 / shape_density as f32))))
+                                    * size_mod
+                                    / 10.,
+                                5,
+                            )
+                            .into(),
                         )
                         .into(),
+                    material: materials.add(ColorMaterial::from(Color::hsla(
+                        i as f32 / shape_density as f32 * 360.0,
+                        2.0,
+                        2.0,
+                        0.50,
+                    ))),
+                    transform: Transform::from_translation(
+                        Quat::from_rotation_z(
+                            ((spiral_num as f32 / total_spirals as f32) * 360.0).to_radians(),
+                        )
+                        .mul_vec3(Vec3::new(f.sin() * radius, f.cos() * radius, 0.) + offset),
                     )
-                    .into(),
-                material: materials.add(ColorMaterial::from(Color::hsla(
-                    i as f32 / shape_density as f32 * 360.0,
-                    2.0,
-                    2.0,
-                    0.50,
-                ))),
-                transform: Transform::from_translation(
-                    Vec3::new(f.sin() * radius, f.cos() * radius, 0.) + offset,
-                )
-                .with_rotation(Quat::from_rotation_z(f)),
-                ..default()
-            });
-        }
-        offset += change * size_mod;
-        direction += 1;
+                    .with_rotation(Quat::from_rotation_z(f)),
+                    ..default()
+                });
+            }
+            offset += change * size_mod;
+            direction += 1;
 
-        if direction > 3 {
-            direction -= 4;
-        }
-        if u == 0 {
-            direction -= 1;
+            if direction > 3 {
+                direction -= 4;
+            }
+            if u == 0 {
+                direction -= 1;
+            }
         }
     }
 }
